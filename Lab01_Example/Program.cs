cbec21b8d363c5782bc52fa5a51672dafa414a16
HudymaValentyn <gudumavalik@gmail.com>
2023-03-18 15:13:36 +0200

    Add Program and Test

--- Lab01_Example/Program.cs
+++ Lab01_Example/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        static public int Example(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + c.ToString());
+            return c;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введiть перше число");
+            string S1 = Console.ReadLine();
+            int a = int.Parse(S1);
+            Console.WriteLine("Введiть друге число");
+            string S2 = Console.ReadLine();
+            int b = int.Parse(S2);
+            Example(a, b);
+            Console.ReadLine();
+
+        }
+    }
+}
